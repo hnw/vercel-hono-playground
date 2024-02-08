b83684b4fe8ba7574167dc94e734724dd098876f
Yoshio HANAWA <y@hnw.jp>
2024-02-08 09:25:38 +0900

    xxx

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import { oidcAuthMiddleware, getAuth, revokeSession, processOAuthCallback } from "/Users/hanawa-y/src/github.com/hnw/middleware/packages/oidc-auth/src";
 
 export const config = {
   runtime: 'edge'
@@ -7,6 +8,14 @@ export const config = {
 
 const app = new Hono().basePath('/api')
 
+app.get('/logout', async (c) => {
+  await revokeSession(c)
+  return c.text(`Logged off`)
+})
+app.get('/callback', async (c) => {
+  return processOAuthCallback(c)
+})
+app.use('/*', oidcAuthMiddleware())
 app.get('/', (c) => {
   return c.json({ message: 'Hello Hono!!' })
 })
